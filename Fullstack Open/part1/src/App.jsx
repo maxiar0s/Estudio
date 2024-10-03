9d5486d86274a2c87d3a562431c2962b813130bb
maxiar0s <maxi.aros.osorio@gmail.com>
2024-10-03 17:43:28 -0300

    backend

--- Fullstack Open/part1/src/App.jsx
+++ Fullstack Open/part1/src/App.jsx
@@ -1,15 +1,61 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Note from "./components/Note";
 
-const App = ({ notes }) => {
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(true);
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/notes").then((response) => {
+      console.log("promise fulfilled");
+      setNotes(response.data);
+    });
+  }, []);
+
+  console.log("render", notes.length, "notes");
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value);
+  };
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    console.log("Crear Nota");
+    console.log(newNote);
+    const addNoteToState = {
+      id: notes.length + 1,
+      content: newNote,
+      important: Math.random() < 0.5,
+    };
+    console.log(addNoteToState);
+    setNotes([...notes, addNoteToState]);
+    setNewNote("");
+  };
+
+  const handleShowAll = () => {
+    setShowAll(() => !showAll);
+  };
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
   return (
     <div>
       <h1>Notes</h1>
+      <button onClick={handleShowAll}>
+        {showAll ? "Show only important" : "Show all"}
+      </button>
       <ul>
-        <li>{notes[0].content}</li>
-        <li>{notes[1].content}</li>
-        <li>{notes[2].content}</li>
+        {notesToShow.map((note) => {
+          return <Note key={note.id} content={note.content} />;
+        })}
       </ul>
+      <form onSubmit={handleClick}>
+        <input type="text" onChange={handleNoteChange} value={newNote} />
+        <button type="submit">Save</button>
+      </form>
     </div>
   );
 };
---
+++ Fullstack Open/part1/src/components/Note.jsx
@@ -0,0 +1,4 @@
+const Note = ({ content }) => {
+  return <li>{content}</li>;
+};
+export default Note;
