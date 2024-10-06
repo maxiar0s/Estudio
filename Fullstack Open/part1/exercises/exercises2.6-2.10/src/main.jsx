f5fe792501f485faed74ff984186c60e412e2281
maxiar0s <maxi.aros.osorio@gmail.com>
2024-10-05 22:03:54 -0300

    exercise 2.11

--- Fullstack Open/part1/exercises/exercises2.6-2.10/src/main.jsx
+++ Fullstack Open/part1/exercises/exercises2.6-2.10/src/main.jsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import axios from "axios";
 
-createRoot(document.getElementById("root")).render(<App />);
+axios.get("http://localhost:3001/people").then((response) => {
+  console.log(response.data);
+  createRoot(document.getElementById("root")).render(
+    <App people={response.data} />
+  );
+});
