908917b2927017230f94c6193bd1898c3442769c
maxiar0s <maxi.aros.osorio@gmail.com>
2024-10-11 20:04:23 -0300

    post and delete using postman

--- Fullstack Open/hola-mundo-node/index.js
+++ Fullstack Open/hola-mundo-node/index.js
@@ -19,6 +19,11 @@ let notes = [
   },
 ];
 
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
 // const app = http.createServer((req, res) => {
 //   res.writeHead(200, { "content-Type": "application/json" });
 //   res.end(JSON.stringify(notes));
@@ -40,6 +45,31 @@ app.get("/api/notes/:id", (req, res) => {
   } else {
     res.status(404).end();
   }
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  notes = notes.filter((note) => note.id !== id);
+  res.status(204).end();
+});
+
+app.use(express.json());
+
+app.post("/api/notes", (req, res) => {
+  const body = req.body;
+
+  if (!body.content) {
+    return res.status(400).json({ error: "content missing" });
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+  res.json(note);
 });
 
 const PORT = 3001;
