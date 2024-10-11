386a44d45a5edeb6780e932d714a6eb089a84e20
maxiar0s <maxi.aros.osorio@gmail.com>
2024-10-11 17:49:58 -0300

    Hola mundo con express

---
+++ Fullstack Open/hola-mundo-node/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+
+// const app = http.createServer((req, res) => {
+//   res.writeHead(200, { "content-Type": "application/json" });
+//   res.end(JSON.stringify(notes));
+// });
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const note = notes.find((note) => note.id === id);
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).end();
+  }
+});
+
+const PORT = 3001;
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
