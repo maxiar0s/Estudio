0dc8c41474c5344ee4c0dc2e5bc45a2cb9ceef0f
maxiar0s <maxi.aros.osorio@gmail.com>
2024-10-11 22:10:21 -0300

    note api rest

--- Fullstack Open/hola-mundo-node/index.js
+++ Fullstack Open/hola-mundo-node/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 let notes = [
   {
@@ -9,7 +10,7 @@ let notes = [
   },
   {
     id: 2,
-    content: "Browser can execute only Javascript",
+    content: "Browser can execute only JavaScript",
     important: false,
   },
   {
@@ -19,59 +20,76 @@ let notes = [
   },
 ];
 
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+app.use(express.json());
+app.use(requestLogger);
+app.use(cors());
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
+
 const generateId = () => {
   const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
   return maxId + 1;
 };
 
-// const app = http.createServer((req, res) => {
-//   res.writeHead(200, { "content-Type": "application/json" });
-//   res.end(JSON.stringify(notes));
-// });
-
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World</h1>");
-});
-
-app.get("/api/notes", (req, res) => {
-  res.json(notes);
-});
-
-app.get("/api/notes/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const note = notes.find((note) => note.id === id);
-  if (note) {
-    res.json(note);
-  } else {
-    res.status(404).end();
-  }
-});
-
-app.delete("/api/notes/:id", (req, res) => {
-  const id = Number(req.params.id);
-  notes = notes.filter((note) => note.id !== id);
-  res.status(204).end();
-});
-
-app.use(express.json());
-
-app.post("/api/notes", (req, res) => {
-  const body = req.body;
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
 
   if (!body.content) {
-    return res.status(400).json({ error: "content missing" });
+    return response.status(400).json({
+      error: "content missing",
+    });
   }
 
   const note = {
     content: body.content,
-    important: Boolean(body.important) || false,
+    important: body.important || false,
     id: generateId(),
   };
 
   notes = notes.concat(note);
-  res.json(note);
+
+  response.json(note);
 });
 
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
+  if (note) {
+    response.json(note);
+  } else {
+    console.log("x");
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
+});
+
+app.use(unknownEndpoint);
+
 const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- Fullstack Open/part1/exercises/Notes APP/src/services/notes.js
+++ Fullstack Open/part1/exercises/Notes APP/src/services/notes.js
@@ -1,12 +1,89 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/notes";
+const baseUrl = "http://localhost:3001/api/notes";
+// const cors = require("cors");
 
-/*
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}*/
+///Middlewares
+// app.use(cors());
 
+// const express = require("express");
+// const app = express();
+
+// let notes = [
+//   {
+//     id: 1,
+//     content: "HTML is easy",
+//     important: true,
+//   },
+//   {
+//     id: 2,
+//     content: "Browser can execute only Javascript",
+//     important: false,
+//   },
+//   {
+//     id: 3,
+//     content: "GET and POST are the most important methods of HTTP protocol",
+//     important: true,
+//   },
+// ];
+
+// const generateId = () => {
+//   const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+//   return maxId + 1;
+// };
+
+// // const app = http.createServer((req, res) => {
+// //   res.writeHead(200, { "content-Type": "application/json" });
+// //   res.end(JSON.stringify(notes));
+// // });
+
+// app.get("/", (req, res) => {
+//   res.send("<h1>Hello World</h1>");
+// });
+
+// app.get("/api/notes", (req, res) => {
+//   res.json(notes);
+// });
+
+// app.get("/api/notes/:id", (req, res) => {
+//   const id = Number(req.params.id);
+//   const note = notes.find((note) => note.id === id);
+//   if (note) {
+//     res.json(note);
+//   } else {
+//     res.status(404).end();
+//   }
+// });
+
+// app.delete("/api/notes/:id", (req, res) => {
+//   const id = Number(req.params.id);
+//   notes = notes.filter((note) => note.id !== id);
+//   res.status(204).end();
+// });
+
+// app.use(express.json());
+
+// app.post("/api/notes", (req, res) => {
+//   const body = req.body;
+
+//   if (!body.content) {
+//     return res.status(400).json({ error: "content missing" });
+//   }
+
+//   const note = {
+//     content: body.content,
+//     important: Boolean(body.important) || false,
+//     id: generateId(),
+//   };
+
+//   notes = notes.concat(note);
+//   res.json(note);
+// });
+
+// const PORT = 3001;
+// app.listen(PORT);
+// console.log(`Server running on port ${PORT}`);
+
+// json-server
 const getAll = () => {
   const request = axios.get(baseUrl);
   const nonExisting = {
